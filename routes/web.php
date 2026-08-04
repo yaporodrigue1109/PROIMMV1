95bf3575a5ebf45d179a28c6fc26e8f44d67eb0e
Justin Dago <@@Soutra@2026@>
2026-08-04 10:03:05 +0000

    Restaure les routes du site web public

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\Auth\AuthController;
+use App\Http\Controllers\Web\WebController;
 
 //Route::get('/', function () {
 //    return view('admin.auth.login');
@@ -10,3 +11,11 @@ use App\Http\Controllers\Admin\Auth\AuthController;
 
 Route::get('test', [AuthController::class, 'showLoginForm'])->name('login');
 
+Route::controller(WebController::class)->group(function () {
+    Route::get('/', 'home')->name('web.home');
+    Route::get('/biens', 'properties')->name('web.properties');
+    Route::get('/tarifs', 'pricing')->name('web.pricing');
+    Route::get('/a-propos', 'about')->name('web.about');
+    Route::get('/contact', 'contact')->name('web.contact');
+    Route::post('/contact', 'sendContact')->name('web.contact.send');
+});
